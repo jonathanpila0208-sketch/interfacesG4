4e31ec892190d6cc7ebfc51b208cae734d9ffebf
Mila <lamieste25@gmail.com>
2026-06-02 13:03:37 -0500

    mi primer commit

---
+++ inicio/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns= [
+    path('', views.hola, name='inicio'),
+    path('index/', views.hola, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('servicios/', views.servicios, name='servicios'),
+    path('booking/', views.booking, name='booking'),
+    path('tecnicos/', views.tecnicos, name='tecnicos'),
+    path('testimonio/', views.testimonio, name='testimonio'),
+    path('login/', views.login_view, name='login')
+]
---
+++ inicio/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.contrib.auth import authenticate, login as auth_login
+
+def hola (request):
+    return render(request,'index.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def servicios(request):
+    return render(request, 'servicios.html')
+
+def booking(request):
+    return render(request, 'booking.html')
+
+def tecnicos(request):
+    return render(request, 'tecnicos.html')
+
+def testimonio(request):
+    return render(request, 'testimonio.html')
+
+def login_view(request):
+    mensaje=''
+    if request.method=='POST':
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        user = authenticate(request,username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+        else:
+            mensaje='Usuario o contraseña incorrectos'
+    return render(request,'login.html',{'mensaje':mensaje})
