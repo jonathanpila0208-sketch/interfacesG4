b7e0f1a78939378b0f2992fa4bb95394f9046916
jesusmontoya3083-oss <jesus.montoya3083@utc.edu.ec>
2026-06-10 13:31:00 -0500

    listar usuarios

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,3 +5,13 @@ def dashboard(request):
     return render(request, 'private/dashboard.html')
 def informe(request):
     return render(request, 'dashboard/informe.html')
+
+from django.contrib.auth.models import User
+
+def listar_usuarios(request):
+
+    usuarios= User.objects.all()
+    contexto = {
+        'usuarios': usuarios
+    }
+    return render (request, 'private/listar_usuarios.html',contexto)
