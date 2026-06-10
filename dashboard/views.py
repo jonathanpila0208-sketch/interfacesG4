bc6f0079d1812de8cc73a1ca3dcf8929cfe71663
Mila <camila.fonseca9296@utc.edu.ec>
2026-06-10 13:27:14 -0500

    Def listar_usuarios

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,5 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request, 'dashboard/dashboard.html')
+
 def informe(request):
     return render(request, 'dashboard/informe.html')
+
+def listar_usuarios(request):
+    return render (request, 'dashboard/listar_usurios.html')# siempre se va a usar dashboard/
