0467530132877f1d68607d33aeb6e565e6cd7302
Jesus Montoya <jesus.montoya3083@utc.edu.ec>
2026-06-11 08:09:45 -0500

    urls/icono

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ urlpatterns=[
     path('',views.dashboard, name='dashboard'),
     path('informe/', views.informe, name='informe'),
     path('listar_usuarios/', views.listar_usuarios, name='listar_usuarios'),
+    path('iconos/', views.iconos, name='iconos'),
 ]
