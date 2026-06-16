93af04b0f0b6289044589299bce0a6b0faf66f89
Mila <camila.fonseca9296@utc.edu.ec>
2026-06-16 13:22:21 -0500

    dyfhwufhwu

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.contrib import messages
+
+from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
 
 def dashboard(request):
@@ -19,3 +21,28 @@ def iconos(request):
 
 def iconos(request):
     return render(request, 'private/informe.html')
+
+def crear_usuarios(request):
+    if request.method == 'POST':
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        password=request.POST.get("password")
+
+        #si no existe el usuario
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "El usuario ya existe")
+            return render(request, "private/crear_usuarios.html")
+        #si no existe el correo
+        if User.objects.filter(email=email).exists():
+            messages.error(request, "El email ya existe")
+
+        #crear usuario
+        User.objects.create_user(
+            username=username,
+            email=email,
+            password=password
+        )    
+        messages.success(request,"Usuario creado con éxito")
+        return redirect (request, "pivate/listar_usuarios.html")
+        return render (request, "pivate/crear_usuarios.html")
+    
