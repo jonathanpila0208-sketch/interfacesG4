9d5e49b950a99d5878185ffadc871b5fa99e9af6
Mila <camila.fonseca9296@utc.edu.ec>
2026-06-23 13:42:34 -0500

    archivos

--- productos/urls.py
+++ productos/urls.py
@@ -3,6 +3,8 @@ from.import views
 
 urlpatterns = [
     path('',views.listar_productos, name='listar_productos'),
-    path('crear_producto',views.crear_productos, name='crear_productos')
+    path('crear_producto',views.crear_productos, name='crear_productos'),
+    path('editar_producto',views.editar_productos, name='editar_productos')
+
 
 ]
