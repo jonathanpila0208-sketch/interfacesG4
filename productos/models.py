a2aba9b538b4508c87a7880db2c4141531982fd2
Mila <camila.fonseca9296@utc.edu.ec>
2026-06-23 12:35:14 -0500

    Models

--- productos/models.py
+++ productos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Producto(models.Model):
+    nombre_producto = models.CharField(max_length=100)
+    precio_producto = models.DecimalField(max_digits=10, decimal_places=2)
+    
