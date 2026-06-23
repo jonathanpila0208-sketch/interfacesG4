2c21c5b6abec31bd1152e3fed7b6fe66c0279dc8
Mila <camila.fonseca9296@utc.edu.ec>
2026-06-23 13:53:44 -0500

    EDITAR

--- productos/urls.py
+++ productos/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from.import views
+from . import views
 
 urlpatterns = [
-    path('',views.listar_productos, name='listar_productos'),
-    path('crear_producto',views.crear_productos, name='crear_productos'),
-    path('editar_producto',views.editar_productos, name='editar_productos')
-
-
+    path('', views.listar_productos, name='listar_productos'),
+    path('crear_producto/', views.crear_productos, name='crear_productos'),
+    path('editar_producto/<int:id>/', views.editar_productos, name='editar_productos'),
+    path('eliminar_producto/<int:id>/', views.eliminar_productos, name='eliminar_producto'),
 ]
