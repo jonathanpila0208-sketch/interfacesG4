6c36cdf3e9bff45452a8f4a474aeaddda1f50adf
Mila <camila.fonseca9296@utc.edu.ec>
2026-06-23 12:37:04 -0500

    Models

--- productos/models.py
+++ productos/models.py
@@ -5,4 +5,5 @@ from django.db import models
 class Producto(models.Model):
     nombre_producto = models.CharField(max_length=100)
     precio_producto = models.DecimalField(max_digits=10, decimal_places=2)
-    
+    stock_producto = models.IntegerField()
+    estado_producto = models.CharField(max_length=20)
