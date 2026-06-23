bd962cdd4eaca9207891d8623ba61fc1a54da5eb
Mila <camila.fonseca9296@utc.edu.ec>
2026-06-23 13:09:28 -0500

    views

--- productos/urls.py
+++ productos/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-    path('',views.listar_productos, name='listar_productos')
+    path('',views.listar_productos, name='listar_productos'),
+    path('crear_producto',views.crear_productos, name='crear_productos')
+
 ]
